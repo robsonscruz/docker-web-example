5f374f301360cf055b634dadd4a0b283e978f75f
Robson Cruz <robs.cruz@gmail.com>
2017-08-30 18:06:08 -0300

    first commit

---
+++ www/api-test/web/app.php
@@ -0,0 +1,13 @@
+<?php
+if (!isset($_SESSION)) {session_start();}
+
+$meuIp = shell_exec("cat /etc/hosts | grep $(cat /etc/hostname) | awk '{print $1}' | uniq");
+
+if (!isset($_SESSION["time"])) {
+	$date             = new \DateTime('now');
+	$_SESSION["time"] = "Santos FC -> ".$date->format("d/m/Y H:i:s");
+}
+
+echo sprintf("Sou API e meu Ip é {$meuIp} e meu time é %s, arquivo fora do container nops =]", $_SESSION["time"]);
+
+phpinfo();
